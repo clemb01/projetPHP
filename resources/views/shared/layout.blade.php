d289121a9dfd0bea4561b9c02a74e35b18eb906f
Jeremy LEDERMANN <jeremy.ledermann17@gmail.com>
2020-12-08 21:44:57 +0100

    Creation modal

--- resources/views/shared/layout.blade.php
+++ resources/views/shared/layout.blade.php
@@ -1,5 +1,6 @@
 <html>
-    <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -9,71 +10,64 @@
     <link rel="stylesheet" href="/css/site.css">
 
     <title>Super site - @yield('title')</title>
-    </head>
-    <body>
-        @section('navbar')
-        <header class="fixed-top" id="header">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                <div class="collapse navbar-collapse" id="navbarColor01">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/accueil">Accueil <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item" style="margin-left: auto; margin-right: auto;">
-                            <form class="form-inline my-2 my-lg-0" action="search" method="get">
-                                <input class="form-control mr-sm-2" type="text" name="query" placeholder="Rechercher un film" required>
-                                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Chercher</button>
-                            </form>
-                        </li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right slices" id="TitreConnection">
-                    <li class='nav-item active' ><a class='nav-link' ><button class="btn btn-secondary my-2 my-sm-0" data-toggle='modal' data-target='#choixuser' type="submit">Se connecter</button></a></li>
-                    </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
-        @show
-        <div class="container" style="margin-top:3.4rem;">
-            <main role="main" class="pb-3">
-                @yield('content')
-            </main>
+</head>
+
+<body>
+    @section('navbar')
+    <header class="fixed-top" id="header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/accueil">Accueil <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item" style="margin-left: auto; margin-right: auto;">
+                        <form class="form-inline my-2 my-lg-0" action="search" method="get">
+                            <input class="form-control mr-sm-2" type="text" name="query" placeholder="Rechercher un film" required>
+                            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Chercher</button>
+                        </form>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right slices" id="TitreConnection">
+                    <li data-toggle='modal' data-target='#SeConnecter' class='nav-item active' onclick="" ><a class="btn btn-default" id="BtnLogin">Se connecter</a></li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+    </header>
+    @show
+    <div class="container" style="margin-top:3.4rem;">
+        <main role="main" class="pb-3">
+            @yield('content')
+        </main>
         </main>
         <div class="modal fade" id="SeConnecter" role="dialog">
-                <div class="modal-dialog" style="max-width: 90%;">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h2 class="modal-title">Nom d'utilisateur</h2>
-                        </div>
-                        <div class="modal-body" id="formulaire">
-                            <div id="">
-                                <span id='NumRef'><p>Référence : </p></span>
-
-                                <div class="row col-sm-12 my-auto" id='ResetBoutonRef'>
-
-                                    <div class="col-sm-6">
-                                        <button name="" id="BtnRefOK" type="button" onclick="changeColor('BtnRefOK')" class="btn btn-lg btn-succes" style="font-size: 200%;" data-dismiss="">OK</button>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <button name="" id="BtnRefNOK" type="button" onclick="changeColor('BtnRefNOK')" class="btn btn-lg" style="font-size: 200%;" data-dismiss="">NOK</button>
-                                    </div>
-
-                                </div>
-                            </div>
-
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" id="BtnChangeUser" onclick="" class="btn btn-default" data-dismiss="modal" style="font-size: 100%;">Se connecter</button>
-                            <button type="button" id="BtnCancel" onclick="" class="btn btn-default" data-dismiss="modal" style="font-size: 100%;">Annuler</button>
-                        </div>
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h2 class="modal-title">Se connecter</h2>
+                    </div>
+                    <div class="modal-body" id="formulaire">
+                        <form class="Jumbotron">
+                            <p><label class="">Login</label></p>
+                            <p><input type="text" value="" id="nom_user" required></p>
+                            <p><label class="">Mot de passe</label></p>
+                            <p><input type="password" value="" id="mdp_user" required></p>
+                            <p>
+                                <button type="submit" onclick="" class="btn btn-default" style="border:solid;">Se connecter</button>
+                                <button onclick="" class="btn btn-default" style="border: solid;">Créer un compte</button>
+                            </p>
+                        </form>
                     </div>
                 </div>
-            </div>           
-       </div>
+            </div>
+        </div>
+    </div>
     <script src="/js/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
     @yield('script')
-        
-    </body>
+
+</body>
+
 </html>
