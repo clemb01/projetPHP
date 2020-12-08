d386f9002d4c3f7bb29d5b59bfa10c48b817cc88
Antoine CACHEUX <coladaitp19-can@ccicampus.fr>
2020-12-08 21:44:56 +0100

    critiques back/front

--- app/Http/Controllers/CommentaireController.php
+++ app/Http/Controllers/CommentaireController.php
@@ -12,7 +12,6 @@ class CommentaireController extends BaseController
     
     public function getUserMovieComms(Request $request)
     {
-<<<<<<< HEAD
     $userComms = DB::select("SELECT * FROM commentaire WHERE film_id = ?", [$request->get('movieId')]);
     $commentaires = array();
     foreach($userComms as $comm)
@@ -21,20 +20,11 @@ class CommentaireController extends BaseController
     }
     
     return view('partial.Comms', ['commentaires' => $commentaires]);
-=======
-    $userRate = DB::select("SELECT contenu FROM commentaire WHERE film_id = ?", [$request->get('movieId')]);
-
-    return view('partial.Comms', ['commentaire' => $userRate[0]->critique]);
->>>>>>> suite critiques/comms
     }
 
     public function saveComms(Request $request)
     {
-<<<<<<< HEAD
         DB::insert("INSERT INTO commentaire (contenu, date, valide, film_id, fk_user_id) values (?, ?, ?, ?, ?)", [$request->get('Message'), date("Y-m-d H:i:s"), 0, $request->get('MovieId'), 1]);
-=======
-        DB::insert("INSERT INTO commentaire (contenu, date, valide, film_id, fk_user_id) values (?, ?, ?, ?, ?)", [$request->get('Message'), date("Y-m-d H:i"), 0, $request->get('MovieId'), 1]);
->>>>>>> suite critiques/comms
     
         $id = $request->get('MovieId');
 
