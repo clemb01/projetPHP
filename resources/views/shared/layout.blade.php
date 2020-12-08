f70e2a0ccc8b80ef11e7f5c42d34e8021d036b28
Jeremy LEDERMANN <jeremy.ledermann17@gmail.com>
2020-12-08 19:03:10 +0100

    Conflit

--- resources/views/shared/layout.blade.php
+++ resources/views/shared/layout.blade.php
@@ -39,6 +39,7 @@
                 @yield('content')
             </main>
         </main>
+<<<<<<< HEAD
         <div class="modal fade" id="SeConnecter" role="dialog">
                 <div class="modal-dialog" style="max-width: 90%;">
                     <div class="modal-content">
@@ -74,5 +75,8 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
     @yield('script')
+=======
+        
+>>>>>>> d73144f... Conflit
     </body>
 </html>
