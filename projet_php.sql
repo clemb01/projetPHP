57683b541173545d29c37a88a964d31e6012fa3b
clemb01 <32745478+clemb01@users.noreply.github.com>
2021-01-05 11:01:48 +0100

    Update projet_php.sql

--- projet_php.sql
+++ projet_php.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : sam. 19 déc. 2020 à 17:29
+-- Généré le : lun. 04 jan. 2021 à 10:28
 -- Version du serveur :  5.7.31
--- Version de PHP : 7.4.9
+-- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,11 +41,7 @@ CREATE TABLE IF NOT EXISTS `commentaire` (
   `Film_logo` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_commentaire` (`fk_user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `commentaire`
---
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -61,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `note` (
   `fk_user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_note` (`fk_user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -79,7 +75,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `login` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `nom`, `prenom`, `role`, `dateN`, `login`, `password`) VALUES
+(9, 'Min', 'Ad', 'admin', '2020-12-09', 'Admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'),
+(10, 'Do', 'Mo', 'modo', '2020-12-23', 'Modo', '04a92cab51b3f445453abdc7b718b4ec5f24e7ee8f2d4ebd7088ae9640461b60'),
+(11, 'Er', 'Us', 'user', '2020-11-11', 'User', '04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb');
 
 --
 -- Contraintes pour les tables déchargées
