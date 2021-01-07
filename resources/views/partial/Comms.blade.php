e2d27983837fb5e48aaf8b877d7de71b30e16dc6
Clément BOUCHON <coladaitp19-bcl@ccicampus.fr>
2021-01-07 09:58:48 +0100

    ajout fiche film profil commentaire

--- resources/views/partial/Comms.blade.php
+++ resources/views/partial/Comms.blade.php
@@ -4,12 +4,27 @@
 <div class="card mb-3">
     <div class="card-body">
         <div class="row no-gutters">
-            <div class="col-md-10"> 
-            @if(str_contains($commentaire->Login(),'UtilisateurSupprimé'))
+            @if(!$isMoviepage)
+            <div class="col-md-2">
+                <a href="/movie/{{ $commentaire->FilmId() }}"><img src="http://image.tmdb.org/t/p/w500{{ $commentaire->Film_logo() }}" class="card-img thumbnail" style="width: 110px;"></a>
+            </div>
+            @endif
+            <div class="col-md-8"> 
+            @if($isMoviepage && str_contains($commentaire->Login(),'UtilisateurSupprimé'))
                 <p class="card-text"><strong>Utilisateur Supprimé</strong> <small>{{$commentaire->Date()}}</small></p>
                 <p class="card-text">{{$commentaire->Contenu()}}</p>
             @else
-                <p class="card-text"><strong><a href="/profil/{{$commentaire->Login()}}">{{$commentaire->Login()}}</a></strong> <small>{{$commentaire->Date()}}</small></p>
+                <p class="card-text">
+                    @if($isMoviepage)
+                    <strong>
+                        <a href="/profil/{{$commentaire->Login()}}">{{$commentaire->Login()}}</a>
+                    </strong>
+                    @else
+                    <strong>
+                        <a href="/movie/{{$commentaire->FilmId()}}">{{$commentaire->Film_titre()}}</a>
+                    </strong><br />
+                    @endif
+                <small>{{$commentaire->Date()}}</small></p>
                 <p class="card-text">{{$commentaire->Contenu()}}</p>
             @endif
             </div>
